b46e5cba88a965576df896bf4f6d7f620577dff7
BrianFernandoJara <brianfernandojara@gmail.com>
2023-08-23 10:53:48 -0400

    dom content loading lab complete

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",()=>{
+    const text = document.getElementById("text");
+    text.innerHTML = "This is really cool!";
+});
+
+console.log("You've disrepected the DOM for the last time.")
